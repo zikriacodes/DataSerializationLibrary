61db2f13be4ffed56064f4bcecec9797f4526335
Zikria Azimi <zikria.azimi@hotmail.com>
2024-10-14 03:24:40 +0200

    add serializers

---
+++ DataSerializationLibrary/JsonSerializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+
+namespace DataSerializationLibrary
+{
+    public class JsonSerializer : IDataSerializer
+    {
+        /// <summary>
+        /// Serializes an object to a JSON string.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>A JSON string representation of the serialized object.</returns>
+        public string Serialize(object obj)
+        {
+            // Check if the object is null to prevent serialization errors
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj), "Cannot serialize a null object.");
+            }
+
+            // Serialize the object to a JSON string
+            return System.Text.Json.JsonSerializer.Serialize(obj);
+        }
+
+        /// <summary>
+        /// Deserializes a JSON string back to an object of specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="data">The JSON string representation of the serialized object.</param>
+        /// <returns>The deserialized object.</returns>
+        public T Deserialize<T>(string data)
+        {
+            // Check if the JSON string is null or empty to prevent deserialization errors
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentException("Cannot deserialize a null or empty JSON string.", nameof(data));
+            }
+
+            // Deserialize the JSON string back to the specified object type
+            return System.Text.Json.JsonSerializer.Deserialize<T>(data);
+        }
+    }
+}
---
+++ DataSerializationLibrary/XmlSerializer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DataSerializationLibrary
+{
+    public class XmlSerializer : IDataSerializer
+    {
+        /// <summary>
+        /// Serializes an object to an XML string.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>An XML string representation of the serialized object.</returns>
+        public string Serialize(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj), "Cannot serialize a null object.");
+            }
+
+            // Create a new StringBuilder to hold the XML data
+            var stringBuilder = new StringBuilder();
+
+            // Use StringWriter to serialize the object to XML
+            using (var stringWriter = new StringWriter(stringBuilder))
+            {
+                var xmlSerializer = new System.Xml.Serialization.XmlSerializer(obj.GetType());
+                xmlSerializer.Serialize(stringWriter, obj);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Deserializes an XML string back to an object of specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="data">The XML string representation of the serialized object.</param>
+        /// <returns>The deserialized object.</returns>
+        public T Deserialize<T>(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentException("Cannot deserialize a null or empty XML string.", nameof(data));
+            }
+
+            // Use StringReader to read the XML data
+            using (var stringReader = new StringReader(data))
+            {
+                var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+                return (T)xmlSerializer.Deserialize(stringReader);
+            }
+        }
+    }
+}
