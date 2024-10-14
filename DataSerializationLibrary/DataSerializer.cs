bcab064171bba71e7f2b01d32b74231457a47232
Zikria Azimi <zikria.azimi@hotmail.com>
2024-10-14 03:24:29 +0200

    add baseclass

---
+++ DataSerializationLibrary/DataSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataSerializationLibrary
+{
+    public interface IDataSerializer
+    {
+        /// <summary>
+        /// Serializes an object to a string format.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>A string representation of the serialized object.</returns>
+        string Serialize(object obj);
+
+        /// <summary>
+        /// Deserializes a string representation of an object back to its original type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="data">The string representation of the serialized object.</param>
+        /// <returns>The deserialized object.</returns>
+        T Deserialize<T>(string data);
+    }
+}
